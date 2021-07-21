64c9fa875632c96de5eee8c30817ffa7fb91537d
Rhys Williams <rhysthomaswilliams@gmail.com>
2021-07-21 21:50:03 +1000

    Initial antlr4 Clausewitz parser

---
+++ source/EconoViz.Cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using Antlr4.Runtime;
+using EconoViz.Parse;
+using System.IO;
+using System.Text;
+
+namespace EconoViz.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var file = File.OpenRead(@"C:\projects\ck3save\assets\saves\Jarl_Ivar_of_the_Isles_867_01_01.ck3");
+            using var fileStream = new StreamReader(file, Encoding.UTF8);
+            var antlrStream = new AntlrInputStream(fileStream);
+
+            var lexer = new ClausewitzLexer(antlrStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new ClausewitzParser(tokenStream);
+
+            var a = parser.content();
+        }
+    }
+}
